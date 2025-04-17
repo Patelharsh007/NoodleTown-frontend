35a4024573992a91721eaa29b7739fbb7753023c
Patelharsh007 <ptlharsh3073@gmail.com>
2025-04-17 14:18:50 +0530

    remove unused slices from store configuration

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,17 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-// import CartSlice from "./slices/CartSlice";
-import AddressSlice from "./slices/AddressSlice";
-import SelectedAddressSlice from "./slices/SelectedAddressSlice";
-// import OrderSlice from "./slices/OrderSlice";
 import AuthUserSlice from "./slices/AuthUserSlice";
 
 const Store = configureStore({
   reducer: {
-    // cart: CartSlice,
-    address: AddressSlice,
-    seletedAddress: SelectedAddressSlice,
-    // order: OrderSlice,
     authUser: AuthUserSlice,
   },
 });

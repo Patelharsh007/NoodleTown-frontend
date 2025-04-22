679a78e4a5ba2debc28beaba2f4e462bf6f14a4a
Patelharsh007 <ptlharsh3073@gmail.com>
2025-04-22 10:53:25 +0530

    remove commented-out form validation logic from LoginForm

--- src/form/LoginForm.tsx
+++ src/form/LoginForm.tsx
@@ -31,25 +31,6 @@ export const LoginForm: React.FC<LoginFormProps> = ({
     password: "",
   });
 
-  // const validateForm = () => {
-  //   const newErrors: Partial<FormData> = {};
-
-  //   if (!formData.email) {
-  //     newErrors.email = "Email is required";
-  //   } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-  //     newErrors.email = "Invalid email format";
-  //   }
-
-  //   if (!formData.password) {
-  //     newErrors.password = "Password is required";
-  //   } else if (formData.password.length < 6) {
-  //     newErrors.password = "Password must be at least 6 characters";
-  //   }
-
-  //   setErrors(newErrors);
-  //   return Object.keys(newErrors).length === 0;
-  // };
-
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData((prev) => ({

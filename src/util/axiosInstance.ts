939fa9ee89d41661eddfa0757bc97b64f5a5cdb1
Patelharsh007 <ptlharsh3073@gmail.com>
2025-04-22 09:56:40 +0530

    add axios instance with response interceptors for error handling

---
+++ src/util/axiosInstance.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { showErrorToast } from "../components/ToastContainer";
+const BASE_URL = process.env.REACT_APP_BACKENDURL;
+
+//---------------------------Axios Instance----------------------------------
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    if (response.data?.status === "error") {
+      const message = response.data.message || "Something went wrong";
+      // showErrorToast(message);
+      return Promise.reject(new Error(message));
+    }
+    return response;
+  },
+  (error) => {
+    const message =
+      error.response?.data?.message || "An unexpected server error occurred";
+    // showErrorToast(message);
+    return Promise.reject(new Error(message));
+  }
+);

a2a6f67339a8d4e91a29f5910efe4d6c121809f0
Patelharsh007 <ptlharsh3073@gmail.com>
2025-04-30 17:33:23 +0530

    enhance restaurant detail component with open status logic and type definitions

--- src/section/restaurant/RestaurantDetail.tsx
+++ src/section/restaurant/RestaurantDetail.tsx
@@ -11,6 +11,7 @@ import {
 import { useQuery } from "@tanstack/react-query";
 import { fetchRestaurantDetailById } from "../../util/util";
 import RestaurantDetailSkeleton from "../../skeleton/RestaurantDetailSkeleton";
+import { RestaurantItem } from "../../types/type";
 
 interface restaurantProps {
   id: string;
@@ -23,7 +24,7 @@ const RestaurantDetail: React.FC<restaurantProps> = ({ id }) => {
     data: restaurant,
     isLoading,
     error,
-  } = useQuery({
+  } = useQuery<RestaurantItem>({
     queryKey: ["restaurantDetails", id],
     queryFn: () => fetchRestaurantDetailById(id),
     staleTime: 10 * 60 * 1000, // 5min
@@ -57,150 +58,174 @@ const RestaurantDetail: React.FC<restaurantProps> = ({ id }) => {
     );
   }
 
+  const getOpenStatus = (openTime: string, closeTime: string) => {
+    const now = new Date();
+    const currentTime = now.toTimeString().split(" ")[0];
+
+    const isOpen =
+      openTime < closeTime
+        ? currentTime >= openTime && currentTime < closeTime
+        : currentTime >= openTime || currentTime < closeTime;
+
+    return (
+      <span style={{ color: isOpen ? "green" : "red" }}>
+        {isOpen ? "Open Now" : "Closed"}
+      </span>
+    );
+  };
+
   return (
     <>
       <Container maxWidth="md" sx={{ marginTop: { xs: "40px" } }}>
         {isLoading ? (
           <RestaurantDetailSkeleton />
         ) : (
-          <Grid2 container spacing={"20px"}>
-            <Grid2
-              size={{ xs: 12, sm: 3 }}
-              display={"flex"}
-              justifyContent={{ xs: "center", sm: "flex-start" }}
-              alignItems={{ xs: "center", sm: "self-start" }}
-            >
-              <Box
-                component={"img"}
-                src={restaurant?.logo}
-                alt={restaurant?.title}
-                width={{ xs: "187px", sm: "160px", md: "187px" }}
-                height={{ xs: "186px", sm: "159px", md: "186px" }}
-              />
-            </Grid2>
-
-            <Grid2 size={{ xs: 12, sm: 9 }} width={"100%"}>
-              <Stack
-                width={"87%"}
-                margin="auto"
-                gap={"15px"}
-                direction={"column"}
+          restaurant && (
+            <Grid2 container spacing={"20px"}>
+              <Grid2
+                size={{ xs: 12, sm: 3 }}
+                display={"flex"}
+                justifyContent={{ xs: "center", sm: "flex-start" }}
+                alignItems={{ xs: "center", sm: "self-start" }}
               >
-                <Typography
-                  fontFamily={"Poppins"}
-                  fontWeight={{ xs: 500, sm: 600 }}
-                  fontSize={{ xs: "28px", sm: "36px" }}
-                  lineHeight={{ xs: "42px", sm: "54px" }}
-                  letterSpacing="0%"
-                  color={"#000000"}
-                  textAlign={{ xs: "center", sm: "left" }}
-                >
-                  {restaurant?.title}
-                </Typography>
+                <Box
+                  component={"img"}
+                  src={restaurant?.logo}
+                  alt={restaurant?.title}
+                  width={{ xs: "187px", sm: "160px", md: "187px" }}
+                  height={{ xs: "186px", sm: "159px", md: "186px" }}
+                />
+              </Grid2>
 
+              <Grid2 size={{ xs: 12, sm: 9 }} width={"100%"}>
                 <Stack
-                  direction={"row"}
-                  justifyContent={"space-between"}
-                  gap="15px"
+                  width={"87%"}
+                  margin="auto"
+                  gap={"15px"}
+                  direction={"column"}
                 >
                   <Typography
                     fontFamily={"Poppins"}
-                    fontWeight={300}
-                    fontSize={{ xs: "13px", sm: "16px" }}
-                    lineHeight={{ xs: "20px", sm: "24px" }}
+                    fontWeight={{ xs: 500, sm: 600 }}
+                    fontSize={{ xs: "28px", sm: "36px" }}
+                    lineHeight={{ xs: "42px", sm: "54px" }}
                     letterSpacing="0%"
-                    color="#999999"
+                    color={"#000000"}
+                    textAlign={{ xs: "center", sm: "left" }}
                   >
-                    {restaurant?.cuisines.join(", ")}
+                    {restaurant?.title}
                   </Typography>
 
-                  <Typography
-                    fontFamily={"Poppins"}
-                    fontWeight={300}
-                    fontSize={{ xs: "13px", sm: "16px" }}
-                    lineHeight={{ xs: "20px", sm: "24px" }}
-                    letterSpacing="0%"
-                    color="#999999"
+                  <Stack
+                    direction={"row"}
+                    justifyContent={"space-between"}
+                    gap="15px"
                   >
-                    {`Average Cost: ${restaurant?.avgCostPerPerson}rs per person`}
-                  </Typography>
-                </Stack>
-
-                <Typography
-                  fontFamily={"Poppins"}
-                  fontWeight={300}
-                  fontSize={{ xs: "13px", sm: "16px" }}
-                  lineHeight={{ xs: "20px", sm: "24px" }}
-                  letterSpacing="0%"
-                  color="#999999"
-                >
-                  {restaurant?.address}
-                </Typography>
-
-                <Typography
-                  fontFamily={"Poppins"}
-                  fontWeight={300}
-                  fontSize={{ xs: "13px", sm: "16px" }}
-                  lineHeight={{ xs: "20px", sm: "24px" }}
-                  letterSpacing="0%"
-                  color="#999999"
-                >
-                  {restaurant && (
-                    <>
-                      <span style={{ color: "red" }}>Open Now</span>{" "}
-                      {restaurant?.timing} (Today)
-                    </>
-                  )}
-                </Typography>
-
-                <Stack
-                  direction={{ xs: "column", sm: "row" }}
-                  gap={{ xs: 2, sm: 3 }}
-                  width="100%"
-                  alignItems={{ xs: "center" }}
-                >
-                  {["Order Online", "Directions", "Share"].map((text) => (
-                    <Button
-                      key={text}
-                      sx={{
-                        width: { xs: "80%", sm: "33%" },
-                        paddingY: { xs: 1, sm: 1.5 },
-                        border: "2px solid #FFA500",
-                        color: "#000000",
-                        "&:hover": {
-                          backgroundColor: "#FFA500",
-                          borderColor: "#FFA500",
-                          "& .MuiTypography-root": {
-                            color: "#FFFFFF",
-                          },
-                        },
-                      }}
-                      onClick={() => {
-                        if (text === "Order Online") {
-                          handleOrderOnline();
-                        }
-                        if (text === "Directions") {
-                          handleDirection();
-                        }
-                        if (text === "Share") {
-                          handleShare();
-                        }
-                      }}
+                    <Typography
+                      fontFamily={"Poppins"}
+                      fontWeight={300}
+                      fontSize={{ xs: "13px", sm: "16px" }}
+                      lineHeight={{ xs: "20px", sm: "24px" }}
+                      letterSpacing="0%"
+                      color="#999999"
                     >
-                      <Typography
-                        fontFamily="Poppins"
-                        fontWeight={500}
-                        fontSize={{ xs: "14px", sm: "16px", md: "18px" }}
-                        color="inherit"
+                      {restaurant?.cuisines.join(", ")}
+                    </Typography>
+
+                    <Typography
+                      fontFamily={"Poppins"}
+                      fontWeight={300}
+                      fontSize={{ xs: "13px", sm: "16px" }}
+                      lineHeight={{ xs: "20px", sm: "24px" }}
+                      letterSpacing="0%"
+                      color="#999999"
+                    >
+                      {`Average Cost: ${Math.round(
+                        restaurant?.avg_cost_per_person
+                      )}rs per person`}
+                    </Typography>
+                  </Stack>
+
+                  <Typography
+                    fontFamily={"Poppins"}
+                    fontWeight={300}
+                    fontSize={{ xs: "13px", sm: "16px" }}
+                    lineHeight={{ xs: "20px", sm: "24px" }}
+                    letterSpacing="0%"
+                    color="#999999"
+                  >
+                    {restaurant?.address}
+                  </Typography>
+
+                  <Typography
+                    fontFamily={"Poppins"}
+                    fontWeight={300}
+                    fontSize={{ xs: "13px", sm: "16px" }}
+                    lineHeight={{ xs: "20px", sm: "24px" }}
+                    letterSpacing="0%"
+                    color="#999999"
+                  >
+                    {restaurant && (
+                      <>
+                        {getOpenStatus(
+                          restaurant?.open_time,
+                          restaurant?.close_time
+                        )}{" "}
+                        ({restaurant?.open_time} - {restaurant?.close_time})
+                        (Today)
+                      </>
+                    )}
+                  </Typography>
+
+                  <Stack
+                    direction={{ xs: "column", sm: "row" }}
+                    gap={{ xs: 2, sm: 3 }}
+                    width="100%"
+                    alignItems={{ xs: "center" }}
+                  >
+                    {["Order Online", "Directions", "Share"].map((text) => (
+                      <Button
+                        key={text}
+                        sx={{
+                          width: { xs: "80%", sm: "33%" },
+                          paddingY: { xs: 1, sm: 1.5 },
+                          border: "2px solid #FFA500",
+                          color: "#000000",
+                          "&:hover": {
+                            backgroundColor: "#FFA500",
+                            borderColor: "#FFA500",
+                            "& .MuiTypography-root": {
+                              color: "#FFFFFF",
+                            },
+                          },
+                        }}
+                        onClick={() => {
+                          if (text === "Order Online") {
+                            handleOrderOnline();
+                          }
+                          if (text === "Directions") {
+                            handleDirection();
+                          }
+                          if (text === "Share") {
+                            handleShare();
+                          }
+                        }}
                       >
-                        {text}
-                      </Typography>
-                    </Button>
-                  ))}
+                        <Typography
+                          fontFamily="Poppins"
+                          fontWeight={500}
+                          fontSize={{ xs: "14px", sm: "16px", md: "18px" }}
+                          color="inherit"
+                        >
+                          {text}
+                        </Typography>
+                      </Button>
+                    ))}
+                  </Stack>
                 </Stack>
-              </Stack>
+              </Grid2>
             </Grid2>
-          </Grid2>
+          )
         )}
       </Container>
     </>

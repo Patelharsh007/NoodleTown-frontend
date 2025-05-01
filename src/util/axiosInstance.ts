ae9c40903bcca6ccab04b59389ca823564308935
Patelharsh007 <ptlharsh3073@gmail.com>
2025-05-01 09:22:57 +0530

    added status code check for erros

--- src/util/axiosInstance.ts
+++ src/util/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { showErrorToast } from "../components/ToastContainer";
+import { showErrorToast, showSuccessToast } from "../components/ToastContainer";
 const BASE_URL = process.env.REACT_APP_BACKENDURL;
 
 //---------------------------Axios Instance----------------------------------
@@ -12,16 +12,37 @@ axiosInstance.interceptors.response.use(
   (response) => {
     if (response.data?.status === "error") {
       const message = response.data.message || "Something went wrong";
-      // showErrorToast(message);
+      // showErrorToast(response.data?.message);
       return Promise.reject(new Error(message));
     }
+    // showSuccessToast(response.data?.message);
     return response;
   },
   (error) => {
-    const message =
-      error.response?.data?.message ||
-      error ||
-      "An unexpected server error occurred";
+    const status = error.response?.status;
+    let message;
+
+    switch (status) {
+      case 400:
+        message = "Bad Request";
+        break;
+      case 401:
+        message = "Unauthorized";
+        break;
+      case 403:
+        message = "Forbidden";
+        break;
+      case 404:
+        message = "Not Found";
+        break;
+      case 500:
+        message = "Internal Server Error";
+        break;
+      default:
+        message = "An unexpected server error occurred";
+    }
+
+    message = error.response?.data?.message || message;
     // showErrorToast(message);
     return Promise.reject(new Error(message));
   }
